b90c4690200e2780333b2bdd87091644f0f5e96c
fu220 <2863318196@qq.com>
2025-11-01 20:13:51 +0800

    fix:Update the setup_mtcp_dpdk_env
    Modify setup_mtcp_dpdk_env.sh to make it compatible to dpdk 24.11.
    
    Signed-off-by: fu220 <2863318196@qq.com>

--- setup_mtcp_dpdk_env.sh
+++ setup_mtcp_dpdk_env.sh
@@ -1,72 +1,90 @@
 #!/usr/bin/env bash
 
 GREEN='\033[0;32m'
-NC='\033[0m'
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
 
-# Get to script directory
 cd $(dirname ${BASH_SOURCE[0]})/
 
-# First download dpdk
-if [ -z "$(ls -A $PWD/dpdk)" ]; then
-    printf "${GREEN}Cloning dpdk...\n $NC"
-    git submodule init
-    git submodule update
-fi
-
-# Setup dpdk source for compilation
+# download dpdk
 if [ "$#" -ne 1 ];
 then
-    export RTE_SDK=$PWD/dpdk
+	export RTE_SDK=$PWD/dpdk
 else
-    export RTE_SDK=$1
-fi
-printf "${GREEN}Running dpdk_setup.sh...\n $NC"
-if grep "ldflags.txt" $RTE_SDK/mk/rte.app.mk > /dev/null
-then
-    :
-else
-    sed -i -e 's/O_TO_EXE_STR =/\$(shell if [ \! -d \${RTE_SDK}\/\${RTE_TARGET}\/lib ]\; then mkdir \${RTE_SDK}\/\${RTE_TARGET}\/lib\; fi)\nLINKER_FLAGS = \$(call linkerprefix,\$(LDLIBS))\n\$(shell echo \${LINKER_FLAGS} \> \${RTE_SDK}\/\${RTE_TARGET}\/lib\/ldflags\.txt)\nO_TO_EXE_STR =/g' $RTE_SDK/mk/rte.app.mk
+	export RTE_SDK=$1
 fi
 
-# Compile dpdk and configure system
-if [ -f $RTE_SDK/usertools/dpdk-setup.sh ]; then
-    bash $RTE_SDK/usertools/dpdk-setup.sh
-else
-    bash $RTE_SDK/tools/setup.sh
+printf "${GREEN}Checking DPDK version...\n$NC"
+
+# Compile DPDK
+if [ -f "$RTE_SDK/meson.build" ]; then
+	printf "${GREEN}Detected new DPDK verision(Meson build system)\n$NC"
+
+	export RTE_TARGET=build
+	BUILD_DIR=$RTE_SDK/$RTE_TARGET
+
+
+	if [ ! -f "$BUUILD_DIR/build.ninja" ]; then
+		printf "${GREEN}Configuring DPDK with Meson...\n$NC"
+		cd $RTE_SDK
+		meson setup $RTE_TARGET
+		cd -
+	fi
+
+	printf "${GREEN}Building DPDK with Ninja...\n$NC"
+	cd $RTE_SDK
+	ninja -C $RTE_TARGET
+	cd -
 fi
 
-# Print the user message
+printf "${GREEN}Setting up environment variables...\n$NC"
 cd $RTE_SDK
-CONFIG_NUM=1
-for cfg in config/defconfig_* ; do
-    cfg=${cfg/config\/defconfig_/}
-    if [ -d "$cfg" ]; then
-	printf "Setting RTE_TARGET as $cfg\n"
-	export RTE_TARGET=$cfg
-    fi
-    let "CONFIG_NUM+=1"
-done
+
+if [ -f "meson.build" ]; then
+	export RTE_TARGET=build
+	export PKG_CONFIG_PATH=$RTE_SDK/$RTE_TARGET/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH
+	export LD_LIBRARY_PATH=$RTE_SDK/$RTE_TARGET/lib/x86_64-linux-gnu:$LD_LIBRARTY_PATH
+fi
+
 cd -
 printf "Set ${GREEN}RTE_SDK$NC env variable as $RTE_SDK\n"
 printf "Set ${GREEN}RTE_TARGET$NC env variable as $RTE_TARGET\n"
 
-# Check if you are using an Intel NIC
+if [ -f "$RTE_SDK/meson.build" ]; then
+	printf "${GREEN}For DPDK appliucations, use: pkg-config --libs --cflags libdpdk\n$NC"
+	printf "${GREEN}Or set: PKG_CONFIG_PATH=$RTE_SDK/$RTE_TARGET/lib/x856_64-linux-gnu/pkgconfig\n$NC"
+fi
+
+# CHeck if you are using an Intel NIC
 while true; do
-    read -p "Are you using an Intel NIC (y/n)? " response
-    case $response in
-	[Yy]* ) break;;
-	[Nn]* ) exit;;
-    esac
+	read -p "Are you using an Inel NIC (y/n)? " response
+	case $response in
+		[Yy]* )
+			printf "Creating ${GREEN}dpdk$NC interface entries\n"
+			if [ -d "dpdk-iface-kmod" ]; then
+				cd dpdk-iface-kmod
+				if [ -f "Makefile" ]; then
+					make
+					if lsmod | grep dpdk_iface &> /dev/null ; then
+						:
+					else
+						sudo insmod ./dpdk_iface.ko
+					fi
+					sudo -E make run
+				fi
+				cd ..
+			else
+				printf "${YELLOW}dpdk-iface-kmod directory not found, skipping interface creation\n$NC"
+			fi
+			break;;
+		[Nn]* )
+			exit;;
+		* )
+			printf "${RED}Please anser y or n\n$NC";;
+	esac
 done
 
-# Create interfaces
-printf "Creating ${GREEN}dpdk$NC interface entries\n"
-cd dpdk-iface-kmod
-make
-if lsmod | grep dpdk_iface &> /dev/null ; then
-    :
-else    
-    sudo insmod ./dpdk_iface.ko
-fi
-sudo -E make run
-cd ..
+printf "${GREEN}DPDK setup completed successfully!\n$NC"
+
+
+
